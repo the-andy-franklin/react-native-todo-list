fddc4658de8ba7d55e99b7d087c4bccf5f922a2f
Andy <the.andy.franklin@gmail.com>
2024-05-06 12:00:21 -0700

    just a bit of clean-up

--- backend/src/env.ts
+++
@@ -1,8 +0,0 @@
-import type { ValueOf } from "./utils/functions/types/ValueOf.ts";
-
-export const EnvVars = {
-	ACCESS_TOKEN_SECRET: "ACCESS_TOKEN_SECRET",
-	REFRESH_TOKEN_SECRET: "REFRESH_TOKEN_SECRET",
-} as const;
-
-export type EnvVars = ValueOf<typeof EnvVars>;
