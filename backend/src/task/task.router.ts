36772a83d70ed1cb5f1548c83332fcd1eb896343
Andy <the.andy.franklin@gmail.com>
2024-07-29 08:11:09 -0700

    wrapped post in Try

--- backend/src/task/task.router.ts
+++ backend/src/task/task.router.ts
@@ -10,7 +10,7 @@ task_router.get("/", async (c) => {
 	const { username }: { username: string } = c.get("jwtPayload");
 	const user = await Try(() => User.findOne({ username }).populate("tasks").exec());
 	if (user.failure) return c.json({ message: user.error.message }, 500);
-	if (!user.data) return c.json({ message: "User not found" }, 400);
+	if (!user.data) return c.json({ message: "Tasks not found" }, 400);
 
 	return c.json(user.data.tasks);
 });
@@ -26,9 +26,11 @@ task_router.get("/:id", async (c) => {
 
 task_router.post("/", taskPostBodyValidatorMiddleware, async (c) => {
 	const { username }: { username: string } = c.get("jwtPayload");
-	const user = await User.findOne({ username }).exec();
-	if (!user) return c.json({ message: "User not found" }, 400);
+	const result = await Try(() => User.findOne({ username }).populate("tasks").exec());
+	if (result.failure) return c.json({ message: result.error.message }, 500);
+	if (!result.data) return c.json({ message: "Tasks not found" }, 400);
 
+	const user = result.data;
 	const body = c.get("body");
 	const new_task = await Try(() => new Task({ ...body, author: user._id }).save());
 	if (new_task.failure) return c.json({ message: new_task.error.message }, 500);
